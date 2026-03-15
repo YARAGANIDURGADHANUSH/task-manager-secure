ea3707cd40a9894cba6d943c68c3410a89c8ea0e
YARAGANI DURGA DHANUSH <ydurgadhanush@gmail.com>
2026-03-15 16:14:47 +0530

    add homepage

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{padding:"40px"}}>
+      <h1>Task Manager Secure</h1>
+
+      <p>Welcome to the Task Manager Application.</p>
+
+      <a href="/register">Register</a>
+      <br/><br/>
+      <a href="/login">Login</a>
+      <br/><br/>
+      <a href="/dashboard">Dashboard</a>
+    </div>
+  )
+}
